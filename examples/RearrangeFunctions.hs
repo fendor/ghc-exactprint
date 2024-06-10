fb80b5a409139f331ed73e3b34d417023c5e508a
Fendor <power.walross@gmail.com>
2024-06-10 17:14:58 +0200

    Update rearrange functions test case

--- examples/RearrangeFunctions.hs
+++ examples/RearrangeFunctions.hs
@@ -1,28 +1,35 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-module RearrangeFunctions where
-
+{-# LANGUAGE PartialTypeSignatures #-}
 -- We parse a module and rearrange the functions depending on their order in the import list.
 
 
+import GHC.Paths
+
 import Language.Haskell.GHC.ExactPrint
-import Language.Haskell.GHC.ExactPrint.Utils
-import HsSyn
-import RdrName
-import SrcLoc
+import Language.Haskell.GHC.ExactPrint.Types
+import Language.Haskell.Syntax.Decls
+import Language.Haskell.Syntax
+import GHC.Hs.Extension
+import GHC.Hs.ImpExp
+import GHC.Types.Name.Reader
+import GHC.Types.SrcLoc
+
 import Data.Maybe
 import qualified Data.Map as Map
 
 main :: IO ()
 main = do
-  Right (as, L l m) <- parseModule "imports.hs"
-  let decls = hsmodDecls m
+  Right (L l hsmod) <- parseModule libdir "examples/imports.hs"
+  let decls = makeDeltaAst $ hsmodDecls hsmod
+  let
       newDecls =
-        case hsmodExports m of
+        case hsmodExports hsmod  of
             Nothing -> decls
-            Just exp ->
-              let cares = map showGhc $ mapMaybe (sortExports . unLoc) (unLoc exp)
+            Just exports ->
+              let
+                  cares = map showGhc $ mapMaybe (sortExports . unLoc) (unLoc exports)
                   mkKey v = maybe "default" showGhc (getKey . unLoc $ v)
                   declMap = foldr (\v -> Map.insertWith (++) (mkKey v) [v]) Map.empty decls
                   (sorted, leftover)
@@ -31,31 +38,31 @@ main = do
                                       Just vs -> (vs ++ r, Map.delete k m)) ([], declMap) cares
               in sorted ++ concatMap snd (Map.toAscList leftover)
   -- We do not need to change the annotations at all
-  putStrLn $ exactPrintWithAnns (L l m { hsmodDecls = newDecls }) as
+  putStrLn $ exactPrint (L l hsmod { hsmodDecls = newDecls })
 
 
 -- | Find the names of things we might care about
-sortExports :: IE RdrName -> Maybe RdrName
-sortExports (IEVar n)  = Just (unLoc n)
-sortExports (IEThingAbs n) = Just (unLoc n)
-sortExports (IEThingAll n) = Just (unLoc n)
-sortExports (IEThingWith n _) = Just (unLoc n)
-sortExports (IEModuleContents _) = Nothing
+sortExports :: IE GhcPs -> Maybe RdrName
+sortExports (IEVar _ n _)  = Just (lieWrappedName n)
+sortExports (IEThingAbs _ n _) = Just (lieWrappedName n)
+sortExports (IEThingAll _ n _) = Just (lieWrappedName n)
+sortExports (IEThingWith _ n _ _ _) = Just (lieWrappedName n)
+sortExports (IEModuleContents _ _) = Nothing
 -- Haddock constructors
 sortExports _ = Nothing
 
 -- Get the key to sort decls by
-getKey :: HsDecl RdrName -> Maybe RdrName
+getKey :: HsDecl GhcPs -> Maybe RdrName
 --getKey (TyClDecl t) = Just (unLoc $ tyClDeclLname  t)
-getKey (SigD t) =
+getKey (SigD _ t) =
   case t of
-       TypeSig (n:_) _ _ -> Just (unLoc $ n)
-       PatSynSig n _ _ _ _ -> Just (unLoc $ n)
+       TypeSig _ (n:_) _ -> Just (unLoc $ n)
+       PatSynSig _ (n:_) _ -> Just (unLoc $ n)
        _ -> Nothing
-getKey (ValD t) =
+getKey (ValD _ t) =
   case t of
        FunBind{fun_id} -> Just (unLoc $ fun_id)
-       PatSynBind b    -> Just (unLoc $ psb_id b)
+       PatSynBind _ b    -> Just (unLoc $ psb_id b)
        -- Can't appear at the top level
        PatBind{} -> Nothing
        -- Renamed
